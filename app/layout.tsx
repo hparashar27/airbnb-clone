e782305f445ac4cd4f7a7c92f5d7561ae3a6b338
Harsh <hparashar617@gmail.com>
2023-04-10 16:28:47 +0530

    Airbnb app setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import {Nunito} from "next/font/google"
+
+const font = Nunito({
+  subsets : ["latin"],
+});
+
+export const metadata = {
+  title: 'Airbnb Clone',
+  description: 'this is full stack Airbnb clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
