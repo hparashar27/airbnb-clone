c5e1d19a37f49b0be177c0bef10fdf86a7701896
Harsh <hparashar617@gmail.com>
2023-04-14 12:07:52 +0530

    Input.tsx

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -7,7 +7,8 @@ import { toast } from "react-hot-toast"; // react-hot-toast is for sending the b
 import { FieldValues, SubmitHandler,useForm } from "react-hook-form";
 import Modal from "./Modal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
-
+import Heading from "../Heading";
+import Input from "../inputs/Input";
 
 const RegisterModal = () => {
 
@@ -29,6 +30,40 @@ const RegisterModal = () => {
         setIsLoading(false);
       })
     }
+    const bodyContent = (
+      <div className="flex flex-col gap-4">
+        <Heading
+          title="Welcome to Airbnb"
+          subtitle="Create an account!"
+        />
+        <Input
+          id="email"
+          label="Email"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <Input
+          id="name"
+          label="Name"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <Input
+          id="password"
+          label="Password"
+          type="password"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+
 return(
   <Modal 
     disabled={isLoading}
